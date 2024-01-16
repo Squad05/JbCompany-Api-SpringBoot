071a902326997115964d148f4056597f0725277c
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-01-16 09:02:18 -0300

    Correção cros usuario

--- src/main/java/com/api/jbcompany/api/controller/UsuarioController.java
+++ src/main/java/com/api/jbcompany/api/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
