2a6673c6bdc54ef52e6706b2f097c9e6e202fb8c
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-01-28 08:36:18 -0300

    Cross candidatura

--- src/main/java/com/api/jbcompany/api/controller/CandidaturaCursoController.java
+++ src/main/java/com/api/jbcompany/api/controller/CandidaturaCursoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +23,14 @@ public class CandidaturaCursoController {
     @Autowired
     private CandidaturaCursoService candidaturaCursoService;
 
+    @CrossOrigin
     @GetMapping("/{cursoId}")
     public ResponseEntity<List<CandidaturaCurso>> listarCandidaturasPorCursoId(@PathVariable Long cursoId) {
         List<CandidaturaCurso> candidaturasCursos = candidaturaCursoService.listarCandidaturaPorCursoId(cursoId);
         return ResponseEntity.ok(candidaturasCursos);
     }
 
+    @CrossOrigin
     @PostMapping
     public ResponseEntity<CandidaturaCurso> cadastrarCandidaturaCurso(@RequestBody CandidaturaCurso candidaturaCurso) {
         CandidaturaCurso novaCandidaturaCurso = candidaturaCursoService.cadastrarCandidatura(candidaturaCurso);
