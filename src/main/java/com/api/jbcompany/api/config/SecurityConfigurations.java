72020d7d610e3cbf0cb2f94a97f204a024a5ed2e
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-01-28 15:54:58 -0300

    Permitir candidaturas contar sem autenticação

--- src/main/java/com/api/jbcompany/api/config/SecurityConfigurations.java
+++ src/main/java/com/api/jbcompany/api/config/SecurityConfigurations.java
@@ -32,6 +32,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.GET, "/aulas").permitAll()
                         .requestMatchers(HttpMethod.GET, "/aulas/*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/cursos/*").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/candidaturas/contar/*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/aulas/curso/*").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/logar").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/cadastrar").permitAll()
